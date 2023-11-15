d75ff8c29d7a91d12ae2b628b0ca82a1b176b0b8
Joshua Hewson <jhewson@login007.oscar.ccv.brown.edu>
2023-11-15 14:46:29 -0500

    lost

--- rl/rl_callibration.py
+++ rl/rl_callibration.py
@@ -33,15 +33,14 @@ def sample(n_trials, distributions, a, b):
     return SimpleRL(n_trials=n_trials, distributions=distributions).simulate(alpha=a, temperature=b)
 
 
-def test_rl_calibration():
-    quantiles = np.zeros((nrep, 2))
-    for rep_i in tqdm(range(nrep)):
-        a_true = np.random.uniform(*a_range)
-        b_true = np.random.uniform(*b_range)
-        y_R, y_C = sample(trials, dists, a_true, b_true)
-        data = fit_pymc(y_R, y_C, prior_tuple)
-        quantiles[rep_i, 0] = np.mean(data.posterior.a > a_true)
-        quantiles[rep_i, 1] = np.mean(data.posterior.b > b_true)
+def rl_calibration():
+    quantiles = np.zeros((2,))
+    a_true = np.random.uniform(*a_range)
+    b_true = np.random.uniform(*b_range)
+    y_R, y_C = sample(trials, dists, a_true, b_true)
+    data = fit_pymc(y_R, y_C, prior_tuple)
+    quantiles[0] = np.mean(data.posterior.a > a_true)
+    quantiles[1] = np.mean(data.posterior.b > b_true)
     return quantiles
 
 
